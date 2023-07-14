41e0e5e58504213d34fa023f583bf129a2b37788
Hugo Schott <hugo.schott@etu.univ-lyon1.fr>
2023-07-14 18:24:12 +0200

    main

--- code/main.cpp
+++ code/main.cpp
@@ -97,7 +97,6 @@ int main(int, char**) {
 
     Mesh mesh_frame = Mesh::Frame();
     Mesh mesh_grid = Mesh::Grid(4);
-    Mesh mesh_sphere = Mesh::Sphere(50);
     //Mesh mesh = Mesh("../ressources/splash/splash.gltf");
     //Mesh mesh = Mesh("../ressources/samples/centurion_helmet.obj");
     //Mesh mesh = Mesh("../ressources/samples/statue.stl");
@@ -146,13 +145,12 @@ int main(int, char**) {
 
         object_shader.setVec3("lightPos", glm::vec3(0, 0, 3));
         object_shader.setVec3("cameraPos", orbiter.Position);
-        object_shader.setVec3("min_vertex", mesh_sphere.min_vertex);
-        object_shader.setVec3("max_vertex", mesh_sphere.max_vertex);
+        object_shader.setVec3("min_vertex", mesh.min_vertex);
+        object_shader.setVec3("max_vertex", mesh.max_vertex);
 
         if(wireframe) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         else glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        //mesh.Draw(object_shader);
-        mesh_sphere.Draw(object_shader);
+        mesh.Draw(object_shader);
 
         debug_shader.use();
         debug_shader.setMat4("model", model);
